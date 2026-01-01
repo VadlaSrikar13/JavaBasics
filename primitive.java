971f13159f78b6a5620fb2fa1f7380dc6b554020
SRIKAR <vsrikar461@gmail.com>
2026-01-01 23:15:28 +0530

    Create primitive.java

---
+++ primitive.java
@@ -0,0 +1,12 @@
+public class Main {
+public static void main(String[] args) {
+int items = 8;
+float costperitem = 8.77f;
+float totalcost = items * costperitem;
+char currency = '$';
+
+System.out.println("Number of items:" + items);
+System.out.println("cost per item:" + cost per item + currency);
+System.out.println("Total cost:" + totalcost + currncy);
+}
+}
