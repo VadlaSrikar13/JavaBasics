b9108f8bd94ef7a95d37c42eba515052cb4d702b
SRIKAR <vsrikar461@gmail.com>
2026-01-01 22:15:10 +0530

    Add Main class with student information display

---
+++ variable.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args){
+    String studentName = "Srikar";
+    int studentID = 16;
+    int studentAge = 18;
+    float studentFee = 1.75f;
+    char studentGrade = 'A';
+
+  System.out.println("student name:" + studentName);
+    System.out.println("student id:" + studentID);
+      System.out.println("student age:" + studentAge);
+        System.out.println("student fee:" + studentFee);
+          System.out.println("student grade:" + studentGrade);
+  }
+}
+    
