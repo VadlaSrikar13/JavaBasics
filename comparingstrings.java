b594ca15ad53d4b6bec1773ff18f31a6a9cc576b
SRIKAR <vsrikar461@gmail.com>
2026-01-07 22:17:59 +0530

    Create comparingstrings.java

---
+++ comparingstrings.java
@@ -0,0 +1,13 @@
+public class Main{
+public static void main(String[] args){
+String txt1 = "hello";
+String txt2 = "hello";
+
+String txt3 = "srikar";
+String txt4 = "sri";
+
+System.out.println(txt1.equals(txt2));
+System.out.println(txt3.equals(txt4));
+
+}
+}
